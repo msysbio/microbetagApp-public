53e9c70ad18c3e5513638afb91a4558556de8f81
Haris <haris.zafeiropoulos@kuleuven.be>
2023-06-27 16:49:36 +0200

    link to db with mysql.connector and query phendb

--- services/web/app.py
+++ services/web/app.py
@@ -1,18 +1,14 @@
 
 from typing import List, Dict
-from flask import Flask, jsonify, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, send_from_directory, request
 import mysql.connector
 import json
 
 app = Flask(__name__)
 
-app.config.from_object("config.Config")
-db = SQLAlchemy(app)
-
 # Database connection configuration
 config = {
-    'user': 'root',
+    'user': 'msysbio',
     'password': 'pass',
     'host': 'db',  # Replace with the actual database service name or IP address
     'port': 3306,  # Replace with the actual port
@@ -20,41 +16,48 @@ config = {
 }
 
 
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
-
-
-def favorite_colors() -> List[Dict]:
+def get_phendb_traits(gtdb_genome_id="GCA_018819265.1") -> List[Dict]:
 
     # Establish a database connection
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
 
-    query = "SELECT * FROM test"
+    query = "".join(["SELECT * FROM phenDB WHERE gtdbId = '", gtdb_genome_id, "';"])
     cursor.execute(query)
+    rows = cursor.fetchall()
 
-    results = []
-    for row in cursor:
-        results.append(row)
-
-    return results
+    return rows
 
 
 @app.route('/')
 def index() -> str:
-    return json.dumps({'favorite_colors': favorite_colors()})
+    return 'Hello friend'
+
+
+@app.route('/phen-traits-db/<string:gtdb_genome_id>', methods=['GET'])
+def phen_traits(gtdb_genome_id):
+    return jsonify(get_phendb_traits(gtdb_genome_id))
+
+
+@app.route('/upload-abundance-table')
+def upload():
+
+    print("hello friend")
+
+    if 'file' not in request.files:
+        return 'No file uploaded', 400
+
+    file = request.files['file']
+    file.save(file.filename)
+
+    # process_file(file.filename)
+
+    return "hello friend"
+    # return 'File uploaded and processed successfully', 200
 
 
 if __name__ == '__main__':
+
     import os
-    # print(os.environ)
 
     app.run()
-    # app.run()
